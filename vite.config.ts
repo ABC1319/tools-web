eaa95665059bd7f80d5228dbf585c37753fd4381
ABC1319 <109276671+ABC1319@users.noreply.github.com>
2026-07-02 18:09:27 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,20 +14,17 @@ export default defineConfig(({command, mode}) => {
     plugins: [
       vue(),
       createSvgIconsPlugin({
+        // Specify the icon folder to be cached
         iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+        // Specify symbolId format
         symbolId: 'icon-[dir]-[name]',
       }),
       seoperender()
     ],
     resolve: {
       alias: {
-        "@": path.resolve("./src"),
-        // 为 v-code-diff 指定正确的入口
-        "v-code-diff": path.resolve(__dirname, "node_modules/v-code-diff/dist/index.js")
+        "@": path.resolve("./src")  //相对路径别名配置， 使用@替代src
       }
-    },
-    optimizeDeps: {
-      include: ['v-code-diff']
     },
     server: {
       host: env.VITE_HOST,
@@ -35,7 +32,13 @@ export default defineConfig(({command, mode}) => {
         [env.VITE_APP_BASE_API] : {
           target: env.VITE_SERVE,
           changeOrigin: true,
+          // bypass(req, res, options) {
+          //   const proxyUrl = new URL(options.rewrite(req.url) || '', (options.target) as string)?.href || ''
+          //   req.headers['x-req-proxyUrl'] = proxyUrl;
+          //   res.setHeader("x-res-proxyUrl", proxyUrl)
+          // }
         },
+        
       }
     }
   }
